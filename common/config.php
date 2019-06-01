32fb43fe3d01f4c0f130f378e5da2a4c6f0fed0f
chixiaocheng <18778525819@163.com>
2019-06-01 14:30:07 +0800

    0.0.2.5

---
+++ common/config.php
@@ -0,0 +1,7 @@
+<?php
+# 基本配置文件 #
+
+// 数据库配置
+include_once "db.php";
+$db = new mysqli($db_host, $db_username, $db_password);
+
--- common/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-# 数据库账号信息 #
-
-$db_host = 'localhost'; //主机
-$db_username = 'root'; //账号
-$db_password = '1245678'; //密码
