957ad43392ca2d470796f9a278aa25956d0a4ecf
chixiaocheng <18778525819@163.com>
2019-06-01 15:46:52 +0800

    0.0.2.14

--- common/config.php
+++ common/config.php
@@ -3,4 +3,7 @@
 
 /* 数据库配置 */
 include_once "db.php"; //数据库账号信息
-$db = new mysqli($db_host, $db_username, $db_password);
+$db = new mysqli($db_host, $db_username, $db_password, $db_name);
+if ($db->connect_error) {
+    die('数据库连接错误！');
+}
--- common/db.php
+++ common/db.php
@@ -1,7 +1,8 @@
 <?php
 ## 数据库账号信息 ##
-// 注意本文件在Git版本管理中忽略更新
+// 注意使本文件在Git版本管理中忽略更新
 
-$db_host = 'localhost'; //主机
-$db_username = 'root'; //账号
+$db_host = 'localhost'; //主机地址
+$db_username = 'amateur'; //账号
 $db_password = '1234567890'; //密码
+$db_name = 'amateur'; //数据库名
