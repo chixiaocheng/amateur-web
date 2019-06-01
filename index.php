3c1675adf7a31b1db9b4972507f6d7c14a93ed26
chixiaocheng <18778525819@163.com>
2019-06-01 17:03:24 +0800

    0.0.2.16

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 ## 索引/全局入口 ##
-echo 'Hello world!';
+
+/* 判断终端类型 (存在优先级) */
+if (isset($_GET['mobile'])) {
+    echo 'mobile';
+}else if (isset($_GET['desktop'])) {
+    echo 'desktop';
+}else if(isset($_GET['wechat'])){
+    echo "wechat";
+}else{
+    echo "wrong";
+}
