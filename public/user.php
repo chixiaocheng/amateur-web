25d3921d1c44f091d46327264a0058afc45a0edb
chixiaocheng <18778525819@163.com>
2019-06-02 21:49:17 +0800

    0.0.3.1

--- public/user.php
+++ public/user.php
@@ -1,7 +1,8 @@
 <?php
 ## 用户信息 ##
+//TODO
 include_once "../common/config.php";
-$res=$db->query("SELECT nickname FROM user WHERE id=1");
+$res = $db->query("SELECT nickname FROM user WHERE id=1");
 $row = $res->fetch_assoc();
 var_dump($row);
 
