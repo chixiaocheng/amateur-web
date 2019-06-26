ccd355bb6c4625362c3017746bd3b0cac7708dd0
chixiaocheng <18778525819@163.com>
2019-06-26 21:03:12 +0800

    0.2.0.3

--- common/Tpl.class.php
+++ common/Tpl.class.php
@@ -15,6 +15,30 @@ class Tpl
     }
 
     function getTpl($type, $model) {
-
+        DB::table('tpl');
+        echo $type . $model;
     }
+
+    /**
+     * 模板实例化编译
+     * 全部编译成单引号
+     * @param string $tpl
+     * @return string
+     */
+    public static function compile($tpl) {
+        $tplc = '<?php $tplc=\'' . $tpl;
+        //$tplc = str_replace(':', '\:', $tplc);
+        $tplc = preg_replace_callback_array(['/<web if=(\'|\")([\s\S]*?)(\'|\")>([\s\S]*?)<\/web>/' => function ($e) { //编译if-else
+            return "'.(('$e[2])?($e[4]):()).'";
+        }, '//' => function ($e) { //编译foreach
+
+        }, '/\{\{([\s\S]*?)\}\}/' => function ($e) { //编译data
+            return "'.\$this->data['{$e[1]}'].'";
+        }], $tplc);
+        $tplc .= "'";
+        echo $tplc;
+        return $tplc;
+    }
+
+
 }
